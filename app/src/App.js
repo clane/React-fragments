7de8e6511c4c8bda07a145c83db891dc651bcf38
Chris Lane <chris.lane359@gmail.com>
2019-01-11 08:32:23 -0800

    multiple fragments are being rendered but the data is missing

--- app/src/App.js
+++ app/src/App.js
@@ -2,32 +2,31 @@ import React, { Component } from 'react';
 import './App.css';
 import { Fragment } from 'react';
 
-class List extends Component {
-  render() {
-    return (
-      <ul>
-          <ListItems />
-      </ul>
-    );
-  }
+function ListItem({ num }) {
+  return (
+    <Fragment>
+      <li>{num}</li>
+    </Fragment>
+  );
 }
 
-class ListItems extends Component {
-  render() {
-    return (
-			<React.Fragment>
-        <li>list item</li>
-      </React.Fragment>
-    );
-  }
+function List(props) {
+  return (
+    <ul>
+      {props.numbers.map(num => (
+        <ListItem item={num}  />
+      ))}
+    </ul>
+  );
 }
+const numbers = [1, 2, 3, 4, 5];
 
 class App extends Component {
   render() {
     return (
 			<div>
 				<h1>React Fragments</h1>
-      	<List />
+      	<List numbers={numbers}/>
 			</div>
     );
   }
